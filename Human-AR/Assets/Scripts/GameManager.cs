c8aeaa1c9c3c757c96b64bef6c6cd97f67010013
Yender-M <myenderenrique@gmail.com>
2022-11-18 20:22:08 -0600

    Todo el menu xd a ver si sirve

---
+++ Human-AR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnMainMenu;
+    public event Action OnItemsMenu;
+    public event Action OnARPosition;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        OnMainMenu?.Invoke();
+    }
+
+    public void ItemsMenu()
+    {
+        OnItemsMenu?.Invoke();
+    }
+
+    public void ARPosition()
+    {
+        OnARPosition?.Invoke();
+    }
+
+    public void CloseAPP()
+    {
+        Application.Quit();
+    }
+}
