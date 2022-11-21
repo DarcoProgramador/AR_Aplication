6d9668d6038e649cfcdde2a74afd8be5668e7efb
DJosuep <josuep.garciar@gmail.com>
2022-11-21 15:59:03 -0600

    Añadidos paneles informativo

--- Human-AR/Assets/Scripts/UIManager.cs
+++ Human-AR/Assets/Scripts/UIManager.cs
@@ -19,21 +19,26 @@ public class UIManager : MonoBehaviour
 
     private void ActivateMainMenu()
     {
+        //Muestra el botón de abrir el menú de objetos y el botón de cerrar
         mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
         mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
 
+        //Oculta el panel deslizable y el botón de cerrar el menú de objetos
         itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOMoveY(180,0.3f);
 
+        //Oculta el botón de aceptar
         ARPositionCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
     }
 
     private void ActivateItemsMenu()
     {
+        //Oculta el botón de abrir el menú de objetos y el botón de cerrar
         mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
         mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
 
+        //Muestra el panel deslizable y el botón de cerrar el menú de objetos
         itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.5f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOMoveY(300, 0.3f);
@@ -41,13 +46,16 @@ public class UIManager : MonoBehaviour
 
     private void ActivateARPosition()
     {
+        //Oculta el botón de abrir el menú de objetos y el botón de cerrar
         mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
         mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
 
+        //Oculta el panel deslizable y el botón de cerrar el menú de objetos
         itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0, 0, 0), 0.5f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0, 0, 0), 0.3f);
         itemsMenuCanvas.transform.GetChild(1).transform.DOMoveY(180, 0.3f);
 
+        //Muestra el botón de aceptar
         ARPositionCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
     }
 }
